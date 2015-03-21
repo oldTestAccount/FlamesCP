52008c7f3cbbb2cea7c93068ca8fe84fb80e8ad2
root <root@flamespanel.(none)>
2015-03-21 19:50:14 +0300

    logout button now works:   controls.php

--- controls.php
+++ controls.php
@@ -12,14 +12,18 @@ if (strpos($status,$stringfound) !== false) {
 <div class="btn-group">
 <a href="startserver.php" target="_blank" class="btn btn-success">Start Server</a>
 <a href="stop.php" disabled="disabled" target="_blank" class="btn btn-warning">Server already stopped!</a>
+<base target="_parent" />
 <a href="index.php?logout" class="btn btn-danger">Log out</a>
+</base>
 </div>
 <?php } else { ?>
 <div class="jumbotron">
 <div class="btn-group">
 <a href="startserver.php" disabled="disabled" target="_blank" class="btn btn-success">Server already started!</a>
 <a href="stop.php" target="_blank" class="btn btn-warning">Stop Server (data is not saved!)</a>
+<base target="_parent" />
 <a href="index.php?logout" class="btn btn-danger">Log out</a>
+</base>
 </div>
 <?php } ?>
 
