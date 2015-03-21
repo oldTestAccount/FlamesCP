1aea3520c1e0996146fb764c8fdcee6a90e4f65e
root <root@flamespanel.(none)>
2015-03-21 18:52:45 +0300

    modified:   README.md 	on-boot daemon starter:   flamescpd 	created installer:   installers/install.sh

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 a lightweight mysql-free control panel for single minecraft servers
 
 ## Notes
-Please move the authentication directory (security) to /var/www/security, if you would like to test this.
-o.php is the console output reader - do not remove it!
-
-The daemons are in the DAEMON directory - please start a screen for each and start them up seperately.
+Please move the authentication directory (security) to 
+/var/www/security, if you would like to test this. o.php is the console output reader - do not remove it! 
+The daemons are in the DAEMON directory - please start a screen for each and start them up seperately. 
 Make sure you run the daemons as root!
-
 Also, please note that your server should be running in the /SERVER directory of the system, and not in the web directory.
+
+## Dependencies
+This control panel requires: 
+- PHP 5.3+ 
+- CentOS 6.x 
+- Apache (other webservers not currently supported - add support if you want) 
+- PHP GD module 
+- NetCat
---
+++ installers/install.sh
@@ -0,0 +1,42 @@
+if [ -z "$1" ]; then
+echo "Usage: ./install.sh --install"
+else
+if [ "$1" == "--install" ]; then
+os="cat /etc/centos-release"
+if [ -z "$os" ]; then
+echo "You must be running CentOS 6.x!"
+else
+if [ ! -z "$os" ]; then
+echo "Installing dependencies..."
+sleep 1
+yum install -y httpd php php-gd nc git zip unzip
+echo "Grabbing latest release..."
+cd /var/www/html
+wget https://github.com/FlamesRunner/FlamesCP/archive/master.zip
+mv master master.zip
+unzip master.zip
+mv /var/www/html/FlamesCP-master/* /var/www/html
+cd /
+mv /var/www/html/DAEMON .
+mv /var/www/html/flamescpd .
+echo "bash /flamescpd" >> /etc/rc.d/rc.local
+mv /var/www/html/security /var/www/security
+mkdir /SERVER
+cd /SERVER
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.3/minecraft_server.1.8.3.jar
+mv minecraft_server.1.8.3.jar minecraft_server.jar
+clear
+echo "Please enter the amount of RAM that your system has. (in MB)"
+read ram
+echo "java -Xmx$ramM -jar minecraft_server.jar nogui" >> start.sh
+echo "cd /SERVER" >> /var/www/startserver
+echo "bash start.sh" >> /var/www/startserver
+sleep 1
+echo "Installation complete! Please run: bash /flamescpd to start the daemon."
+else
+echo "Invalid option."
+echo "Usage: ./install.sh --install"
+fi
+fi
+fi
+fi
