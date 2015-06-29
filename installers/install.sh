0c3664f3941c2f37916cf052eba9cf9623bcedca
FlamesRunner <akyle2001@gmail.com>
2015-06-28 23:26:57 -0400

    deleted (no longer required):    filemanager/

--- installers/install.sh
+++ installers/install.sh
@@ -6,7 +6,8 @@ else
 if [ ! -z "$os" ]; then
 echo "Installing dependencies..."
 sleep 1
-yum install -y httpd php php-gd nc git zip unzip screen gcc make gc sudo java7
+yum install epel-release
+yum install -y httpd php php-gd nc git zip unzip screen gcc make gc sudo java7 vsftpd
 echo "Grabbing latest release..."
 cd /var/www/html
 wget https://github.com/FlamesRunner/FlamesCP/archive/master.zip
@@ -38,7 +39,37 @@ sleep 1
 service httpd start
 cp /var/www/html/init /etc/init.d/flamescpd
 chmod 755 /etc/init.d/flamescpd
+rm -rf /etc/vsftpd/vsftpd.conf
+cp /var/www/html/vsftpd.conf /etc/vsftpd
+useradd -d /SERVER ftpuser
+chown ftpuser:apache /SERVER
+chown ftpuser:apache /SERVER/*
+chmod 770 /SERVER/*
+clear
+echo "Please enter a password for the FTP user."
+read password
+spawn passwd ftpuser
+expect "Password:"
+send "$password\r"
+expect "Retype new password:"
+send "$password\r"
+interact
+clear
+echo " "
 echo "Please edit /var/www/security/password_protect.php and find the login details. You may change them too."
 echo "Installation complete! Please run: service flamescpd start to start the daemon."
+echo " "
+echo "-----------------------------------------------------------------------------"
+echo " "
+echo "Default administrator details:"
+echo "Username: admin"
+echo "Password: t5uqazeMEp"
+echo " "
+echo "-----------------------------------------------------------------------------"
+echo " "
+echo "You may modify server details through FTP with the details:"
+echo "Username: ftpuser"
+echo "Password: $password"
+echo "IP: <yourserverIPaddress>"
 fi
 fi
