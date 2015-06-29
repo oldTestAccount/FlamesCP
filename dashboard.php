2db713fee87831a966b4f7fa90957a3b5f709ffc
FlamesRunner <akyle2001@gmail.com>
2015-06-29 17:19:07 -0400

    modified:   dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 session_start();
 include('session.php');
 ?>
-$
+
 <body STYLE="background-color:transparent">
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
