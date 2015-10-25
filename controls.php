9bce1d50ad103152d3a0fe4c73b5e018807bbfc9
Andrew H <FlamesRunner@users.noreply.github.com>
2015-10-24 22:24:59 -0400

    Removed ugly flashing

--- controls.php
+++ controls.php
@@ -2,19 +2,11 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <body STYLE="background-color:transparent">
 <?php
-
 include('session.php');
-
-$status = shell_exec("cat /SERVER/logs/latest.log");
-
-$stringfound = 'Stopped';
-
-if (strpos($status,$stringfound) !== false) {
-
 ?>
 <div class="btn-group">
 <a href="startserver.php" target="_blank" class="btn btn-success">Start Server</a>
-<a href="stop.php" disabled="disabled" target="_blank" class="btn btn-warning">Server already stopped!</a>
+<a href="stop.php" target="_blank" class="btn btn-warning">Server already stopped!</a>
 <a href="password.php" class="btn btn-primary">Change password</a>
 <a href="adduser.php" class="btn btn-info">Add user</a>
 <a href="rmuser.php" class="btn btn-default">Remove user</a>
@@ -24,19 +16,4 @@ if (strpos($status,$stringfound) !== false) {
 </div>
 <br> </br>
 <p>Hello! You're currently logged in as: <b><?php echo $_SESSION['logged_in_as']; ?></b>, and your permissions are set to: <?php echo $_SESSION['rank']; ?>.</p>
-<?php } else { ?>
-<div class="btn-group">
-<a href="startserver.php" disabled="disabled" target="_blank" class="btn btn-success">Server already started!</a>
-<a href="stop.php" target="_blank" class="btn btn-warning">Stop Server (data is not saved!)</a>
-<a href="password.php" class="btn btn-primary">Change password</a>
-<a href="adduser.php" class="btn btn-info">Add user</a>
-<a href="rmuser.php" class="btn btn-default">Remove user</a>
-<base target="_parent" />
-<a href="logout.php" class="btn btn-danger">Log out</a>
-</base>
-</div>
-<br> </br>
-<p>Hello! You're currently logged in as: <b><?php echo $_SESSION['logged_in_as']; ?></b> and your permissions are set to: <?php echo $_SESSION['rank']; ?>.</p>
-<?php } ?>
-
 </div>
