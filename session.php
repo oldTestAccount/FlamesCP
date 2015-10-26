17a6942371898ae9c71ab535b4134ba3a96363e0
1Conan <1Conan@users.noreply.github.com>
2015-10-26 20:25:28 +0800

    Update session.php

--- session.php
+++ session.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['logged_in'])){
-header('Location: index.php?error=You must be logged in to view this page.');
+header('Location: index.php?error=2');
 } else {
 
 }
